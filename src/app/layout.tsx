67db4fee275b4c54b157635f05abf0d70ff05aae
srihari20031 <sriharivelayutham.28@gmail.com>
2024-06-30 17:05:57 +0530

    Final Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,36 @@
-"use client";
+
 import "./styles/globals.css";
 import "atropos/css";
 import React from "react";
 import StarsCanvas from "./components/main/StarsCanvas";
+import { Metadata } from "next";
 
 
-// export const metadata: Metadata = {
-//   title: "Sriharinarayan V",
-//   description: "My portfolio",
-// };
 
+export const metadata: Metadata = {
+  metadataBase: new URL("https://srihari.com/"),
+  title: "Sri hari narayan - SDE | SE ",
+  description: "Full Stack Developer with 2+ years of expertise in MERN stack applications. Specializing in dynamic and responsive web development using modern JavaScript technologies and frameworks.",
+   
+  keywords: [
+    "Developer",
+    "Portfolio",
+    "Developer Portflio",
+    "Sri hari",
+    "Next.js",
+    "React",
+    "MERN"
+  ],
+  openGraph: {
+    title: "Sri hari narayan - SDE | SE ",
+    description:
+      "Full Stack Developer with 2+ years of expertise in MERN stack applications. Specializing in dynamic and responsive web development using modern JavaScript technologies and frameworks.",
+    images: "/3d-Ma.png",
+  },
+  alternates: {
+    canonical: "https://srihari.com/",
+  },
+};
 
 
 export default function RootLayout({
